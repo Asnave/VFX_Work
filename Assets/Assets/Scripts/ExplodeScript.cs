2bab60c76e6eb4c66523af02c82dab1a3ff61adb
Abigail Evans <cdn_abigail@outlook.com>
2022-11-14 09:35:05 -0400

    (Added) Smoke Trail
    - When the object explodes the fractured pieces have individual smoke trails
    
    - Edited the explosion to have more force

--- Assets/Assets/Scripts/ExplodeScript.cs
+++ Assets/Assets/Scripts/ExplodeScript.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class ExplodeScript : MonoBehaviour
 {
+    // Credit to Gabriel Aguiar Prod. on Youtube.com for the code and general guidelines on making this effect
+
     public GameObject explosion;
     public Vector3 explosionOffset;
+    public GameObject smokeTrail;
+    public int maximumSmoke;
     public float destroyDelay;
     public float minForce;
     public float maxForce;
@@ -19,11 +23,15 @@ public class ExplodeScript : MonoBehaviour
     public void Explode()
     {
 
+        int smokeCounter = 0;
+
         if (explosion != null)
         {
             GameObject explosionFX = Instantiate(explosion, transform.position + explosionOffset, Quaternion.identity) as GameObject;
             Destroy(explosionFX, 5);
         }
+
+
         foreach (Transform t in transform)
         {
             var rb = t.GetComponent<Rigidbody>();
@@ -33,6 +41,16 @@ public class ExplodeScript : MonoBehaviour
                 rb.AddExplosionForce(Random.Range(minForce, maxForce), transform.position, radius);
             }
 
+            if(smokeTrail != null && smokeCounter < maximumSmoke)
+            {
+                if (Random.Range (1, 4) == 1)
+                {
+                    GameObject smokeFX = Instantiate(smokeTrail, t.transform) as GameObject;
+                    smokeCounter++;
+                    Destroy(smokeFX, 5);
+                }
+
+            }
             Destroy(t.gameObject, destroyDelay);
         }
     }
